7ba5322b91454751bb3f14d28ccc4e2a13cc1870
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2022-10-03 12:01:26 +0530

    First commit

---
+++ AR Object Viewer/AR Object Viewer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR Object Viewer
+//
+//  Created by Rohan Bimal Raj on 03/10/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
