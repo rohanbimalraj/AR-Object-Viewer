d4c0d10d37d59b80c110748b1ba1c6d2acc6f6d5
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2022-10-08 18:25:27 +0530

    Added menu view to project.

---
+++ AR Object Viewer/AR Object Viewer/MenuUIView.swift
@@ -0,0 +1,13 @@
+//
+//  MenuUIView.swift
+//  AR Object Viewer
+//
+//  Created by Rohan Bimal Raj on 08/10/22.
+//
+
+import UIKit
+
+class MenuUIView: UIView {
+
+
+}
--- AR Object Viewer/AR Object Viewer/ViewController.swift
+++ AR Object Viewer/AR Object Viewer/ViewController.swift
@@ -11,11 +11,15 @@ import Combine
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var expandAndCollapseImageView: UIImageView!
+    @IBOutlet weak var menuView: MenuUIView!
+    @IBOutlet weak var menuViewBottomContraint: NSLayoutConstraint!
     @IBOutlet var arView: ARView!
     var cancellable:AnyCancellable? = nil
+    var isMenuOpen:Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUp()
         let anchor = AnchorEntity(world: [0,0,-0.5])
         
         cancellable = Entity.loadAsync(named: "AirForce")
@@ -26,5 +30,35 @@ class ViewController: UIViewController {
                 self.arView.scene.addAnchor(anchor)
                 self.cancellable?.cancel()
             }
+    }
+    
+    func setUp() {
+        setUpTapGesture()
+        menuViewBottomContraint.constant = -(view.bounds.height * 0.45) + 52
+    }
+    
+    func setUpTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTapped))
+        expandAndCollapseImageView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func onTapped() {
+        expandAndCollapseImageView.isUserInteractionEnabled = false
+        if isMenuOpen {
+            menuViewBottomContraint.constant = -(view.bounds.height * 0.45) + 52
+        }else {
+            menuViewBottomContraint.constant = 0
+        }
+        UIView.animate(withDuration: 0.75) {
+            self.view.layoutIfNeeded()
+            if self.isMenuOpen {
+                self.expandAndCollapseImageView.transform = CGAffineTransformIdentity
+            }else {
+                self.expandAndCollapseImageView.transform = CGAffineTransform(rotationAngle: .pi)
+            }
+        }completion: { _ in
+            self.isMenuOpen.toggle()
+            self.expandAndCollapseImageView.isUserInteractionEnabled = true
+        }
     }
 }
