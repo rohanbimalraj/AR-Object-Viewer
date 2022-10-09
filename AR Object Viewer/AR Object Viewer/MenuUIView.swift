7d96e465b5e083d235d2d070e2617e7242f46e00
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2022-10-09 21:18:16 +0530

    Removed UI related code to menu view.

--- AR Object Viewer/AR Object Viewer/MenuUIView.swift
+++ AR Object Viewer/AR Object Viewer/MenuUIView.swift
@@ -8,6 +8,46 @@
 import UIKit
 
 class MenuUIView: UIView {
+    
+    @IBOutlet weak var expandAndCollapseImageView: UIImageView!
+    @IBOutlet weak var menuViewBottomContraint: NSLayoutConstraint!
+    var isMenuOpen:Bool = false
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUp()
+
+    }
+    
+    func setUp() {
+        setUpTapGesture()
+        print("Rohan's:",UIScreen.main.bounds.height)
+        menuViewBottomContraint.constant = -(UIScreen.main.bounds.height * 0.45) + 52
+    }
+    
+    func setUpTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTapped))
+        expandAndCollapseImageView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func onTapped() {
+        expandAndCollapseImageView.isUserInteractionEnabled = false
+        if isMenuOpen {
+            menuViewBottomContraint.constant = -(UIScreen.main.bounds.height * 0.45) + 52
+        }else {
+            menuViewBottomContraint.constant = 0
+        }
+        UIView.animate(withDuration: 0.75) {
+            self.superview?.layoutIfNeeded()
+            if self.isMenuOpen {
+                self.expandAndCollapseImageView.transform = CGAffineTransformIdentity
+            }else {
+                self.expandAndCollapseImageView.transform = CGAffineTransform(rotationAngle: .pi)
+            }
+        }completion: { _ in
+            self.isMenuOpen.toggle()
+            self.expandAndCollapseImageView.isUserInteractionEnabled = true
+        }
+    }
 
 }
